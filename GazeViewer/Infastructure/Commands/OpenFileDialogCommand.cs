542b431c502b1e5f654556f7b30f04de411a5aa4
Денис Черный <userden4ikwork@gmail.com>
2021-11-26 00:49:53 +0300

    IEnumerable Test

---
+++ GazeViewer/Infastructure/Commands/OpenFileDialogCommand.cs
@@ -0,0 +1,24 @@
+﻿using GazeViewer.Infastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace GazeViewer.Infastructure.Commands
+{
+    internal class OpenFileDialogCommand : Command
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+        }
+    }
+}
