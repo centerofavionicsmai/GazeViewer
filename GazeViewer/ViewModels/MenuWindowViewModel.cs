31cf2fedfeba4b439f8cc76a50b20d5d88cefb2d
Денис Черный <userden4ikwork@gmail.com>
2021-11-25 20:53:59 +0300

    Add Menu Window

---
+++ GazeViewer/ViewModels/MenuWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GazeViewer.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazeViewer.ViewModels
+{
+   internal class MenuWindowViewModel : ViewModel
+    {
+        private string _Title = "Menu";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #region SliderSettings
+        //Int используется специально, так как мы идем по List<GazePoint>
+        private int _MaxSliderValue = 75000;
+        public int MaxSliderValue
+        {
+            get => _MaxSliderValue;
+            set => Set(ref _MaxSliderValue, value);
+        }
+
+        private int _MinSliderValue = 0;
+        public int MinSliderValue
+        {
+            get => _MinSliderValue;
+            set => Set(ref _MinSliderValue, value);
+        }
+
+        private int _SliderValue;
+        public int SliderValue
+        {
+            get => _SliderValue;
+            set => Set(ref _SliderValue, value);
+        }
+        #endregion
+
+
+
+    }
+}
